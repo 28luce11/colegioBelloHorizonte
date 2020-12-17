9b5814e9cc26da0993c866513dce69cdeea7d494
28luce11 <luce_tenis@hotmail.com>
2020-12-17 14:21:35 -0500

    comments fix

--- src/app/layouts/external/external-routing.module.ts
+++ src/app/layouts/external/external-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { ExternalComponent } from './external.component';
 
 const routes: Routes = [
--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { ExternalComponent } from './external/external.component';
 import { InternalComponent } from './internal/internal.component';
-import { RouterModule } from '@angular/router';
 import { InternalRoutingModule } from './internal/internal-routing.module';
 import { ExternalRoutingModule } from './external/external-routing.module';
+
 @NgModule({
     declarations: [ExternalComponent, InternalComponent],
     imports: [
