6bf5dc73e1c16a3a0027ae0fe3e3da50ad311f2f
28luce11 <luce_tenis@hotmail.com>
2021-01-06 10:49:39 -0500

    solving errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { LayoutsModule } from './layouts/layouts.module';
-
 
 @NgModule({
     declarations: [
