7b4cdc51b9a724f1e50704dda825d52dcddf2297
28luce11 <luce_tenis@hotmail.com>
2021-01-15 10:25:50 -0500

    resolved errors

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [],
   exports: [
     MatMenuModule,
     MatIconModule,
-    MatCardModule,
-    MatSidenavModule
+    MatCardModule
   ]
 })
 export class MaterialModule { }
